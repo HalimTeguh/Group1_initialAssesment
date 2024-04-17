7b54f994f8fcd08d449388c4fe8c9b3cbb67c270
Halim Teguh S <halimteguhsaputro@gmail.com>
2024-04-17 21:43:15 +0700

    fix: :bug: Fix: Image asset quran

--- lib/main.dart
+++ lib/main.dart
@@ -15,17 +15,16 @@ class HomePage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Row(mainAxisAlignment: MainAxisAlignment.center, children: []),
-              SizedBox(height: 10),
               Stack(
                 alignment: Alignment.center,
                 children: [
                   Container(
-                    margin: EdgeInsets.symmetric(horizontal: 25),
+                    margin: const EdgeInsets.symmetric(horizontal: 25),
                     width: double.infinity,
                     height: 300,
+                    alignment: Alignment.bottomRight, // menambahkan alignment
                     decoration: BoxDecoration(
-                      gradient: LinearGradient(
+                      gradient: const LinearGradient(
                         end: Alignment.centerRight,
                         begin: Alignment.centerLeft,
                         colors: <Color>[
@@ -33,12 +32,12 @@ class HomePage extends StatelessWidget {
                           Color(0xff9055FF),
                         ],
                       ),
-                      color: Color(0xFFDF98FA),
+                      color: const Color(0xFFDF98FA),
                       borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
+                      boxShadow: const [
                         BoxShadow(
                           color: Colors.black54,
-                          offset: const Offset(
+                          offset: Offset(
                             5.0,
                             5.0,
                           ),
@@ -47,89 +46,95 @@ class HomePage extends StatelessWidget {
                         ), //BoxShadow
                       ],
                     ),
-                  ),
-                  Positioned(
-                    bottom: 0,
-                    right: 25,
                     child: Container(
                       width: 265,
                       height: 162,
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                              fit: BoxFit.fill,
-                              image: AssetImage('assets/Quran.png'))),
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/Quran.png'),
+                          // remove fit
+                        ),
+                      ),
                     ),
                   ),
                   Container(
                     child: Column(
                       children: [
-                        Text(
+                        const Text(
                           "Al-Fatihah",
-                          style: TextStyle(color: Colors.white, fontSize: 26, fontFamily: "Poppins"),
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 26,
+                            fontFamily: "Poppins",
+                          ),
                         ),
-                        Text(
+                        const Text(
                           "The Opening",
                           style: TextStyle(
                             color: Colors.white,
                             fontSize: 16,
-                            fontFamily: "Poppins"
+                            fontFamily: "Poppins",
                           ),
                         ),
-                        SizedBox(
+                        const SizedBox(
                           height: 10,
                         ),
                         Container(
-                            width: 200,
-                            child: Divider(
-                              color: Colors.white,
-                            )),
-                        SizedBox(
+                          width: 200,
+                          child: const Divider(
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(
                           height: 10,
                         ),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Text(
+                            const Text(
                               "MECCAN",
                               style: TextStyle(
                                 color: Colors.white,
                                 fontSize: 14,
-                                fontFamily: "Poppins"
+                                fontFamily: "Poppins",
                               ),
                             ),
-                            SizedBox(
+                            const SizedBox(
                               width: 10,
                             ),
                             Container(
                               width: 8,
                               height: 8,
-                              decoration: BoxDecoration(
-                                  color: Color.fromARGB(90, 255, 255, 255),
-                                  shape: BoxShape.circle),
+                              decoration: const BoxDecoration(
+                                color: Color.fromARGB(90, 255, 255, 255),
+                                shape: BoxShape.circle,
+                              ),
                             ),
-                            SizedBox(
+                            const SizedBox(
                               width: 10,
                             ),
-                            Text(
+                            const Text(
                               "7 VERSE",
                               style: TextStyle(
                                 color: Colors.white,
                                 fontSize: 14,
-                                fontFamily: "Poppins"
+                                fontFamily: "Poppins",
                               ),
                             ),
                           ],
                         ),
-                        SizedBox(
+                        const SizedBox(
                           height: 20,
                         ),
                         Container(
                           width: 214,
                           height: 48,
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                                  fit: BoxFit.fill,
-                                  image: AssetImage('assets/bismillah.png'))),
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                              fit: BoxFit.fill,
+                              image: AssetImage('assets/bismillah.png'),
+                            ),
+                          ),
                         ),
                       ],
                     ),
